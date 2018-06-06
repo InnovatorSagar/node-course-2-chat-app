a02ccaa6e024a2e9cb546f9e22c7df7bf0fc82db
Sagat Tyagi <tyagisagar79@gmail.com>
2018-06-06 20:44:36 +0530

    Commiting after adding CSS

--- public/js/chat.js
+++ public/js/chat.js
@@ -13,7 +13,6 @@ function scrollToBottom () {
 
   if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
     messages.scrollTop(scrollHeight);
-    //console.log('Scroll kr bc');
   }
 }
 
